ebdb0d51ce5ddd32553f801b9c3227c85924f28a
Isaac Garcia <garcia_isaac01@student.smc.edu>
2026-07-02 12:43:34 -0700

    Add factorial function

--- math.js
+++ math.js
@@ -13,9 +13,22 @@ function isEven(n){
     return n % 2 == 0;
 }
 
+// Takes in a number n and returns its absolute value
 function abs(n) {
     if(n < 0){
         n *= -1;
     }
     return n;
+}
+
+// Takes in a number n and returns n factorial
+function factorial(n){
+    if(n < 0)
+        return undefined;
+
+    let ans = 1;
+    for (let i = 1; i <= n; i++) {
+        ans *= i;
+    }
+    return ans;
 }
